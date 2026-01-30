bcb34eda955f601ad71c58aa698d808d928e11f6
silenthill99 <florian.graziani@sfr.fr>
2026-01-30 13:55:55 +0100

    Mise à jour du projet

--- resources/js/Pages/Update.tsx
+++ resources/js/Pages/Update.tsx
@@ -15,7 +15,7 @@ type FormProps = {
 
 const Update = () => {
 
-    const {video} = usePage<SharedData & {video: FormProps}>().props
+    const {video} = usePage<SharedData & { video: FormProps }>().props
 
     const {data, setData, post, reset} = useForm<Required<FormProps>>({
         id: video.id,
@@ -33,28 +33,32 @@ const Update = () => {
     return (
         <PageStructure title={"Mettre à jour"} className={"container mx-auto p-5 md:p-4"}>
             <form method={"POST"} onSubmit={handleSubmit}>
-                <Input type={"hidden"} value={data.id} onChange={(e) => setData("id", parseInt(e.target.value))}/>
-                <Label htmlFor={"title"}>Titre de la vidéo</Label>
-                <Input
-                    placeholder={"Titre de la vidéo"}
-                    name={"title"}
-                    id={"title"}
-                    value={data.title}
-                    onChange={(e) => setData("title", e.target.value)}
-                    required
-                />
-                <br/>
-                <Label>URL de la vidéo</Label>
-                <Input
-                    type={"text"}
-                    name={"link"}
-                    id={"link"}
-                    value={data.link}
-                    onChange={(e) => setData("link", e.target.value)}
-                    required
-                />
-                <br/>
-                <Button type={"submit"}>Modifier</Button>
+                <div className={"space-y-4"}>
+                    <Input type={"hidden"} value={data.id} onChange={(e) => setData("id", parseInt(e.target.value))}/>
+                    <div>
+                        <Label htmlFor={"title"}>Titre de la vidéo</Label>
+                        <Input
+                            placeholder={"Titre de la vidéo"}
+                            name={"title"}
+                            id={"title"}
+                            value={data.title}
+                            onChange={(e) => setData("title", e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <Label>URL de la vidéo</Label>
+                        <Input
+                            type={"text"}
+                            name={"link"}
+                            id={"link"}
+                            value={data.link}
+                            onChange={(e) => setData("link", e.target.value)}
+                            required
+                        />
+                    </div>
+                    <Button type={"submit"}>Modifier</Button>
+                </div>
             </form>
         </PageStructure>
     );
