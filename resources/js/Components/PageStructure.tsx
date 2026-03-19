b7e4411a99cf7b7ec27fc04f255b89447e44c70b
silenthill99 <florian.graziani@sfr.fr>
2026-03-19 13:40:30 +0100

    Mise à jour du projet

--- resources/js/Components/PageStructure.tsx
+++ resources/js/Components/PageStructure.tsx
@@ -3,17 +3,31 @@ import {Head} from "@inertiajs/react";
 import Header from "@/Components/Header";
 import Footer from "@/Components/Footer";
 
+const defaultDescription = "Un site pour venir en aide aux personnes victimes de harcèlement scolaire et inciter à libérer la parole.";
+
 type StructureProps = {
     children: React.ReactNode;
     title: string;
+    description?: string;
     className?: string;
 }
 
 const PageStructure = (props: StructureProps) => {
+    const description = props.description || defaultDescription;
+
     return (
         <>
             <section className={"min-h-screen flex flex-col"}>
-                <Head title={props.title} />
+                <Head>
+                    <title>{props.title}</title>
+                    <meta head-key="description" name="description" content={description} />
+                    <meta head-key="og:title" property="og:title" content={props.title} />
+                    <meta head-key="og:description" property="og:description" content={description} />
+                    <meta head-key="og:type" property="og:type" content="website" />
+                    <meta head-key="twitter:card" name="twitter:card" content="summary" />
+                    <meta head-key="twitter:title" name="twitter:title" content={props.title} />
+                    <meta head-key="twitter:description" name="twitter:description" content={description} />
+                </Head>
                 <Header/>
                 <main className={`grow ${props.className}`}>
                     {props.children}
