162ade3037dc8539f14df71283f903a947e76a93
silenthill99 <florian.graziani@sfr.fr>
2026-03-19 14:43:39 +0100

    Mise à jour du SEO

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
-    return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,6 +17,7 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
         $middleware->web(append: [
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
+            SecurityHeaders::class,
         ]);
 
         //
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\ClipController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VideoController;
-use App\Mail\MessageSendMail;
-use App\Models\Message;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -14,31 +11,29 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
-})->name("accueil");
+})->name('accueil');
 
-Route::get("/forum", function() {
+Route::get('/forum', function () {
     return Inertia::render('Forum');
-})->name("forum");
+})->name('forum');
 
-Route::get("/contacts", [MessageController::class, "create"])->name("contacts");
+Route::get('/contacts', [MessageController::class, 'create'])->name('contacts');
 
-Route::post("/contacts", [MessageController::class, "store"])->name("contacts.store");
+Route::post('/contacts', [MessageController::class, 'store'])->name('contacts.store');
 
-Route::resource('/clips', ClipController::class)->only('create')->middleware(["auth", "can:add-video"]);
+Route::resource('/clips', ClipController::class)->only('create')->middleware(['auth', 'can:add-video']);
 Route::resource('/clips', ClipController::class)->except('create');
 
 Route::get('/__mw', fn () => 'ok')->middleware(['auth', 'can:add-video']);
 
-Route::get("/bonus", function () {
+Route::get('/bonus', function () {
     return Inertia::render('Bonus');
-})->name("bonus");
+})->name('bonus');
 
-Route::get("/quelques_films", function () {
+Route::get('/quelques_films', function () {
     return Inertia::render('Quelques films');
-})->name("films");
+})->name('films');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -49,27 +44,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource("videos", VideoController::class)->except('show', 'index');
+    Route::resource('videos', VideoController::class)->except('show', 'index');
 });
 
-Route::resource("videos", VideoController::class)->only('index');
+Route::resource('videos', VideoController::class)->only('index');
 
-//Route::get("/add", [VideoController::class, 'create'])
+// Route::get("/add", [VideoController::class, 'create'])
 //    ->name('videos.create')->middleware(['auth', 'verified', 'can:add-video']);
-//Route::post("/add", [VideoController::class, "store"])->middleware(['auth', 'verified'])->name('videos.store');
+// Route::post("/add", [VideoController::class, "store"])->middleware(['auth', 'verified'])->name('videos.store');
 //
-//Route::get("/update/{id}", [VideoController::class, "edit"])->middleware(['auth', 'verified'])->name('videos.edit');
-//Route::post("/update/{id}", [VideoController::class, "update"])->middleware(['auth', 'verified'])->name('videos.update');
+// Route::get("/update/{id}", [VideoController::class, "edit"])->middleware(['auth', 'verified'])->name('videos.edit');
+// Route::post("/update/{id}", [VideoController::class, "update"])->middleware(['auth', 'verified'])->name('videos.update');
 
-
-
-//Route::get("/test", function () {
+// Route::get("/test", function () {
 //    return new MessageSendMail();
-//})->name("mail.message-send-mail");
-
-Route::get("/test", function () {
-    $msg = new Message();
-    return new MessageSendMail($msg);
-});
+// })->name("mail.message-send-mail");
 
 require __DIR__.'/auth.php';
